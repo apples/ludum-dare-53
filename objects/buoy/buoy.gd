60cbf14e59f3b0c720d9edc49ca93e29e67915df
Apples <dbralir@gmail.com>
2023-04-29 00:49:30 -0500

    Fixed buoy damping

--- objects/buoy/buoy.gd
+++ objects/buoy/buoy.gd
@@ -5,6 +5,7 @@ var stationary_angle = 0
 var spring_factor = 10
 var angular_spring_factor = 250
 var damping_factor = 25
+var tangent_damping_factor = 0.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,7 +27,12 @@ func apply_angular_spring():
 func apply_linear_spring():
 	var current_pos = Vector2(self.position.x, self.position.y)
 	var stationary_pos = Vector2(stationary_pos_x, stationary_pos_y)
-	var direction = current_pos.direction_to(stationary_pos)
 	if current_pos != stationary_pos:
-		var result = -(current_pos - stationary_pos) * spring_factor - linear_velocity.project(direction) * damping_factor 
+		var direction = current_pos.direction_to(stationary_pos)
+		var spring_force = (stationary_pos - current_pos) * spring_factor
+		var damping_force = -linear_velocity.project(direction) * damping_factor
+		var tangent = direction.orthogonal()
+		var tangent_velocity = linear_velocity.project(tangent)
+		var tangent_force = -tangent_velocity * tangent_damping_factor
+		var result = spring_force + damping_force + tangent_force
 		apply_force(result)
