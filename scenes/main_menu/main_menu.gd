892afcaa3e1bc00543c7a39f6bcd453b666c0415
Jacob <jacob.wallace@utexas.edu>
2023-04-29 13:57:33 -0700

    theme includes label changes for font, added online mode toggle

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -8,4 +8,7 @@ func _process(delta):
 	pass
 
 func _on_play_button_pressed():
-	get_tree().change_scene_to_file(gameplay_scene)
+	if $online_mode.button_pressed:
+		print("open modal")
+	else:
+		get_tree().change_scene_to_file(gameplay_scene)
