0f7dd26176b199bd1773d33db0443bbbac47feab
Apples <dbralir@gmail.com>
2023-04-29 16:04:15 -0500

    Player movement tweaks

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -1,5 +1,6 @@
 extends Node
 
+var start_impulse = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,3 +14,9 @@ func _process(delta):
 
 func _on_docking_station_ship_docked(ship_body, anchor_point, anchor_rotation):
 	%PlayerShip.anchor_to(anchor_point, anchor_rotation)
+
+
+func _on_start_timer_timeout():
+	$PlayerShip.enable_input = true
+	$PlayerShip.apply_impulse(Vector2.RIGHT * start_impulse)
+	$StartTimer.queue_free()
