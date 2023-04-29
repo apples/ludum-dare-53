e18ff70709d99e8f8e3e65631f98b98dbc8a245b
Jacob <jacob.wallace@utexas.edu>
2023-04-29 14:42:50 -0700

    confirm online mode

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -3,18 +3,13 @@ var gameplay_scene = "res://scenes/gameplay/gameplay.tscn"
 
 func _ready():
 	$online_mode_details/username_input.text = "Player_%s" %[create_random_client_id()]
-	print(create_random_client_id())
-	pass
-
-func _process(delta):
 	pass
 
 func _on_play_button_pressed():
-	if $online_mode.button_pressed:
+	if $online_mode_toggle.button_pressed:
 		$online_mode_details.visible = true
-		print("open modal")
 	else:
-		get_tree().change_scene_to_file(gameplay_scene)
+		switch_to_gameplay_scene()
 
 func create_random_client_id(): 
 	const chars = "FGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
@@ -25,6 +20,13 @@ func create_random_client_id():
 		
 	return clientId
 
+func switch_to_gameplay_scene():
+	get_tree().change_scene_to_file(gameplay_scene)
 
-func _on_details_cancel_button_pressed():
+func _on_online_mode_confirm_pressed():
+	print("pass username to autoload globals")
+	switch_to_gameplay_scene()
+	
+
+func _on_online_mode_cancel_pressed():
 	$online_mode_details.visible = false
