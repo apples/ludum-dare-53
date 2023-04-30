b61cc6d611c37a5887559e5c9721069973d38ea8
Adam Howard <ahoward0920@gmail.com>
2023-04-29 23:43:58 -0400

    Adding a progress bar

---
+++ scenes/gameplay/ColorRect.gd
@@ -0,0 +1,15 @@
+extends ColorRect
+
+var end_position: float = 4000 - 240
+var max_width: int = 40
+@onready var player_ship = get_node("/root/Gameplay/PlayerShip")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.size.x = max(1, int(player_ship.global_position.x / end_position * max_width))
+	
