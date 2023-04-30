2456c191a8ccaa869df4a3835f5896f5a55b9a0c
Jacob <jacob.wallace@utexas.edu>
2023-04-29 19:23:09 -0700

    username label if cached shows up

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -8,6 +8,7 @@ func _ready():
 		$online_mode_details/username_input.text = "Player_%s" %[create_random_client_id()]
 	else:
 		$online_mode_details/username_input.text = Configs.username
+		$logged_in_username_label.text = Configs.username
 	check_api_sever()
 	
 func check_api_sever():
@@ -16,6 +17,9 @@ func check_api_sever():
 		$online_mode_toggle.disabled = false
 		$online_mode_toggle.button_pressed = true
 		$spinner_wheel.visible = false
+		if Configs.username != null:
+			$logged_in_as_label.visible = true
+			$logged_in_username_label.visible = true
 	else:
 		$spinner_wheel.visible = false
 
@@ -53,3 +57,12 @@ func _on_online_mode_cancel_pressed():
 
 func _on_username_input_text_changed(new_text):
 	$online_mode_details/username_validation_label.visible = false
+
+
+func _on_online_mode_toggle_toggled(button_pressed):
+	if button_pressed:
+		$logged_in_as_label.visible = true
+		$logged_in_username_label.visible = true
+	else:
+		$logged_in_as_label.visible = false
+		$logged_in_username_label.visible = false
