7b7583fc75aa28e26226faa392721a01d9a03e70
Jacob <jacob.wallace@utexas.edu>
2023-04-30 13:50:38 -0700

    buoy tune up - using anchor script now

--- objects/buoy/buoy.gd
+++ objects/buoy/buoy.gd
@@ -37,31 +37,31 @@ func _process(delta):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
-	apply_linear_spring()
-	apply_angular_spring()
+#	apply_linear_spring()
+#	apply_angular_spring()
 	
 	if tape_neighbor:
 		_reconcile_tape()
 
-func apply_angular_spring():
-	var current_angle = self.rotation
-#	var direction = sign(current_angle - stationary_angle)
-	if current_angle != stationary_angle:
-		var result = -(current_angle - stationary_angle) * angular_spring_factor # - angular_velocity * direction * damping_factor 
-		apply_torque(result)
-
-func apply_linear_spring():
-	var current_pos = Vector2(self.position.x, self.position.y)
-	var stationary_pos = Vector2(stationary_pos_x, stationary_pos_y)
-	if current_pos != stationary_pos:
-		var direction = current_pos.direction_to(stationary_pos)
-		var spring_force = (stationary_pos - current_pos) * spring_factor
-		var damping_force = -linear_velocity.project(direction) * damping_factor
-		var tangent = direction.orthogonal()
-		var tangent_velocity = linear_velocity.project(tangent)
-		var tangent_force = -tangent_velocity * tangent_damping_factor
-		var result = spring_force + damping_force + tangent_force
-		apply_force(result)
+#func apply_angular_spring():
+#	var current_angle = self.rotation
+##	var direction = sign(current_angle - stationary_angle)
+#	if current_angle != stationary_angle:
+#		var result = -(current_angle - stationary_angle) * angular_spring_factor # - angular_velocity * direction * damping_factor 
+#		apply_torque(result)
+#
+#func apply_linear_spring():
+#	var current_pos = Vector2(self.position.x, self.position.y)
+#	var stationary_pos = Vector2(stationary_pos_x, stationary_pos_y)
+#	if current_pos != stationary_pos:
+#		var direction = current_pos.direction_to(stationary_pos)
+#		var spring_force = (stationary_pos - current_pos) * spring_factor
+#		var damping_force = -linear_velocity.project(direction) * damping_factor
+#		var tangent = direction.orthogonal()
+#		var tangent_velocity = linear_velocity.project(tangent)
+#		var tangent_force = -tangent_velocity * tangent_damping_factor
+#		var result = spring_force + damping_force + tangent_force
+#		apply_force(result)
 
 
 func _reconcile_tape():
