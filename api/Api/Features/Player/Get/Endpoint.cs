1b525f7a976dad357d84bf7694928fbe26ad4fee
Anthony Stewart <anthony.l.stewart@outlook.com>
2023-04-29 21:18:46 -0500

    Added Player Get endpoint.

---
+++ api/Api/Features/Player/Get/Endpoint.cs
@@ -0,0 +1,28 @@
+﻿using Api.Extensions;
+using Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace Api.Features.Player.Get;
+
+public class Endpoint : Endpoint<Request>
+{
+    public override void Configure()
+    {
+        Get("/player");
+    }
+
+    public override async Task HandleAsync(Request request, CancellationToken cancellationToken)
+    {
+        var claimUserName = User.FindFirstValue<string>("UserName");
+        
+        if (claimUserName == request.UserName)
+        {
+            await SendOkAsync(cancellationToken);
+        }
+        else
+        {
+            await SendNotFoundAsync(cancellationToken);
+        }
+    }
+}
