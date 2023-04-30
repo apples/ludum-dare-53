f712d2887256582d53efd47094ea0dcd82e04550
WiredOverload <mike.everywhere@gmail.com>
2023-04-30 03:50:19 -0500

    moved and added assets

---
+++ objects/trash/large_trash/large_trash.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var rng = RandomNumberGenerator.new()
+	match (rng.randi_range(0, 0)):
+		0:
+			$Sprite2D.texture = load("res://objects/trash/large_trash/longstrutjunk.png")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
