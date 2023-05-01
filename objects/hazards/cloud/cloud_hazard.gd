63a409ad266f6137f8fe3526c9755472a6fae4ef
WiredOverload <mike.everywhere@gmail.com>
2023-05-01 05:04:18 -0500

    spawned hazards

--- objects/hazards/cloud/cloud_hazard.gd
+++ objects/hazards/cloud/cloud_hazard.gd
@@ -1,16 +1,21 @@
 extends Area2D
 
 @export var damage_time: float = .5
+@export var rotation_speed:float = 50
 
+var linear_velocity: Vector2
 var _influenced_objects: Array[RigidBody2D]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var rng = RandomNumberGenerator.new()
+	rotation_speed = rng.randf_range(25, 50)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	position += linear_velocity * delta
+	rotation_degrees += rotation_speed * delta
 	for obj in _influenced_objects:
 		obj.gas_accumulation += delta
 		if obj.gas_accumulation > damage_time:
