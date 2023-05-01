bf3ea3fe3acf03ed29e904e92ca83bdb1d93a63f
Jacob <jacob.wallace@utexas.edu>
2023-05-01 00:47:45 -0700

    oob zap

--- objects/oob_bot/oob_bot.gd
+++ objects/oob_bot/oob_bot.gd
@@ -6,6 +6,8 @@ var angle = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$OutOfBoundsBotAnim.play("default")
+	$ZapAnim.play("default")
+	$ZapAnim.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -13,3 +15,11 @@ func _process(delta):
 		angle += delta * 2.5
 		self.global_position = player_ship.global_position + (Vector2.RIGHT).rotated(angle) * follow_distance
 		self.look_at(player_ship.global_position)
+
+
+func _on_zap_attack_timer_timeout():
+	$ZapAnim.visible = true
+	$ZapAttackVisibleTimer.start()
+
+func _on_zap_attack_visible_timer_timeout():
+	$ZapAnim.visible = false
