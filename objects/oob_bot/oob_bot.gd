a879de7faa785a8e90d4e5ce8b73929c806f4365
Jacob <jacob.wallace@utexas.edu>
2023-04-30 23:59:06 -0700

    oob bot animations and basic scripting done

---
+++ objects/oob_bot/oob_bot.gd
@@ -0,0 +1,15 @@
+extends Node2D
+var player_ship: RigidBody2D = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$OutOfBoundsBotAnim.play("default")
+
+var follow_distance = 50
+var angle = 0
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if player_ship:
+		angle += delta * 2.5
+		self.global_position = player_ship.global_position + (Vector2.RIGHT).rotated(angle) * follow_distance
+		self.look_at(player_ship.global_position)
