bfa0b45b8a099dfa249eeccb9ec557f8858c9b14
Jacob <jacob.wallace@utexas.edu>
2023-05-01 00:29:58 -0700

    oob bot appears and disappears when you cross the boundaries

--- objects/oob_bot/oob_bot.gd
+++ objects/oob_bot/oob_bot.gd
@@ -1,12 +1,12 @@
 extends Node2D
 var player_ship: RigidBody2D = null
+var follow_distance = 50
+var angle = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$OutOfBoundsBotAnim.play("default")
 
-var follow_distance = 50
-var angle = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if player_ship:
