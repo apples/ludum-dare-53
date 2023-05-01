865f26b56a8aed10f79b97893d3667d2b19c22d8
Jacob <jacob.wallace@utexas.edu>
2023-05-01 12:49:39 -0700

    deploy ui tracks changes now

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -10,6 +10,24 @@ signal exit_deploy()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	deploy_menu_root.visible = false
+	render_deploy_list()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func _deploy_item(key: String):
+	if not key in Deployables.infos:
+		return
+	
+	deploy_item.emit(key)
+	
+func render_deploy_list():
+	for c in deployment_list.get_children():
+		c.queue_free()
+		deployment_list.remove_child(c)
+	
 	for k in SaveGame.current.inventory:
 		if SaveGame.current.inventory[k] > 0 and k in Deployables.infos:
 			var item = iv_item_scene.instantiate()
@@ -22,23 +40,13 @@ func _ready():
 		%NoDeployablesLabel.visible = true
 	else:
 		%NoDeployablesLabel.visible = false
-	
-	deploy_menu_root.visible = false
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
-func _deploy_item(key: String):
-	if not key in Deployables.infos:
-		return
-	
-	deploy_item.emit(key)
-	
 
 func _on_deploy_menu_root_exit():
 	exit_deploy.emit()
 
 func _on_player_ship_player_ship_damaged(amount):
 	%PlayerHealthBar.frame = 15 - amount
+
+func _on_deploy_menu_root_visibility_changed():
+	if deploy_menu_root and deploy_menu_root.visible:
+		render_deploy_list()
