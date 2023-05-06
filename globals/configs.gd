ee25345f60cebb8900c442e812f33848d9f997ff
Apples <dbralir@gmail.com>
2023-05-06 14:15:55 -0500

    Actual offline mode

--- globals/configs.gd
+++ globals/configs.gd
@@ -1,13 +1,23 @@
 extends Node
+var offline_mode = false
 var username = null
 var user_key = null
 
 #const filename = "user://ld53.cfg"
 const filename = "user://ld53_config.json"
 
+func _ready():
+	self.load()
+
 func save():
 	var config_file = FileAccess.open(filename, FileAccess.WRITE)
-	config_file.store_string(JSON.stringify({ username = username, user_key = user_key }))
+	var data = JSON.stringify({
+		offline_mode = offline_mode,
+		username = username,
+		user_key = user_key,
+	})
+	print("Saving configs: ", data)
+	config_file.store_string(data)
 	config_file.close()
 	print("Saved config")
 	
@@ -15,6 +25,8 @@ func load():
 	if FileAccess.file_exists(filename):
 		var config_file = FileAccess.open(filename, FileAccess.READ)
 		var cfg = JSON.parse_string(config_file.get_as_text())
+		if "offline_mode" in cfg:
+			offline_mode = cfg.offline_mode
 		username = cfg.username
 		user_key = cfg.user_key
 		print("Loaded config: ", cfg)
